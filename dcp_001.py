0e268b2d68bd8ac120bff37462373de56f113785
asmferr <53370731+asmferr@users.noreply.github.com>
2020-01-05 12:09:41 +0100

    Update dcp_001.py

--- dcp_001.py
+++ dcp_001.py
@@ -11,19 +11,18 @@ Bonus: Can you do this in one pass?
 
 def find_numbers(target, list_numbers):
 	# create set to store the complement of each number in the list 
-	dict_numbers = set()
+	set_numbers = set()
 	
 	for num in list_numbers:
-		# If the complement is in the list two numbers from the list add up to k
-		if target - num in list_numbers:
+		# If the current num is in the list two numbers from the list add up to k
+		if num in set_numbers:
 			return True
 		else:
 			# Store complement
-			dict_numbers.add(target - num)
+			set_numbers.add(target - num)
 
 	return False
 
 
 assert find_numbers(17, [10, 15, 3, 7]) == True
 assert find_numbers(19, [10, 15, 3, 7]) == False
-
